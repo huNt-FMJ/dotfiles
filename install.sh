9fbf936e4347214e223239b9e35d8ea1535781ff
Michael Jäger <M@J.com>
2019-08-28 16:10:59 +0200

    Add install file

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+function testFunction() {
+  echo "param 0: $0"
+  echo "param 1: $1"
+  echo "param 2: $2"
+}
+
+function replaceDotFile() {
+  if test -e $1; then
+    echo $1 already exists
+    echo "backing up old $1 file (~/$1_old)"
+    mv ~/$1 ~/dotfile/$1_old
+    ln -s ./dotfiles/$1 ~/$1
+  else
+    ln -s ./dotfiles/$1 ~/$1
+  fi
+}
+
+
+
+testFunction 1 2
+replaceDotFile .tmux.conf
+replaceDotFile .vimrc
+
+echo list of files having been backuped
+ls -al ~/*_old
+
+
